87bb4ad74019c6323fa50cbd09d461df7fb27b5c
My Phung Tieu <tmp03099@gmail.com>
2022-08-27 23:07:04 -0400

    updated

---
+++ complete/src/main/java/com/example/restservice/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController{
+
+    @GetMapping("/")
+    public String index(){
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/open/child")
+    public String newPage(){
+        return ("New child page!");
+    }
+
+}
