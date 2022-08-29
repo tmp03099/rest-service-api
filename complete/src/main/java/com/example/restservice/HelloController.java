a76cea57521b6b236e7862985040a0fb2d80c5d5
My Phung Tieu <tmp03099@gmail.com>
2022-08-29 17:40:34 -0400

    updated secured app

--- complete/src/main/java/com/example/restservice/HelloController.java
+++ complete/src/main/java/com/example/restservice/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController{
 
-    @GetMapping("/")
+    @GetMapping("/abc")
     public String index(){
         return "Greetings from Spring Boot!";
     }
--- complete/src/test/java/com/example/restservice/HelloControllerTest.java
+++ complete/src/test/java/com/example/restservice/HelloControllerTest.java
@@ -12,6 +12,8 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+//unit test
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
